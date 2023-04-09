e48d781e22817c5e10333c54334c2e7751adbe3b
Semias <Stefan.gall08@gmail.com>
2023-04-09 21:08:36 +0200

    setup tailwind.config and start developing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    // colors: {
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   'white': '#ffffff',
+    //   'purple': '#3f3cbb',
+    //   'midnight': '#121063',
+    //   'metal': '#565584',
+    //   'tahiti': '#3ab7bf',
+    //   'silver': '#ecebff',
+    //   'bubble-gum': '#ff77e9',
+    //   'bermuda': '#78dcca',
+    // },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
