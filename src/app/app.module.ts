5f34bdef33fb7430e54ee7c98941d3f81ff79135
Semias <Stefan.gall08@gmail.com>
2023-04-10 01:04:32 +0200

    first look and initialize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { NoteListComponent } from './note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailViewComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule
