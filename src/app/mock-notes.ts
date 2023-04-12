80283af2f8efdc242db2e6ed37eb767bd4573a09
Semias <Stefan.gall08@gmail.com>
2023-04-13 00:18:24 +0200

    general app styling

--- src/app/mock-notes.ts
+++ src/app/mock-notes.ts
@@ -19,4 +19,22 @@ export const NOTES: Note[] = [
     content: 'Lorem Ipsum',
     tags: ['Arbeit'],
   },
+  {
+    id: 1,
+    title: 'Erste Notiz',
+    content: 'Lorem Ipsum',
+    tags: ['Arbeit', 'Freizeit'],
+  },
+  {
+    id: 2,
+    title: 'Zweite Notiz',
+    content: 'Lorem Ipsum',
+    tags: ['Arbeit', 'Sport'],
+  },
+  {
+    id: 3,
+    title: 'Dritte Notiz',
+    content: 'Lorem Ipsum',
+    tags: ['Arbeit'],
+  },
 ];
