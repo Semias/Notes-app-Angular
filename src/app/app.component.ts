4aa4d7ee65b4282bb8fa603b1b2b3e1cde2772dd
Semias <Stefan.gall08@gmail.com>
2023-05-30 23:45:20 +0200

    Adding Angular Material and Chips Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component, Input, Output } from "@angular/core";
-import { NoteService } from "src/app/services/note.service";
 import { Note } from "./Note";
+import { COMMA, ENTER } from "@angular/cdk/keycodes";
+import { Component } from "@angular/core";
+import { MatChipEditedEvent, MatChipInputEvent, MatChipsModule } from "@angular/material/chips";
 
+export interface Fruit {
+	name: string;
+}
+
+/**
+ * @title Chips with input
+ */
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
@@ -10,4 +18,44 @@ import { Note } from "./Note";
 export class AppComponent {
 	selectedNote?: Note | undefined;
 	filterValue: string = "";
+
+	addOnBlur = true;
+	readonly separatorKeysCodes = [ENTER, COMMA] as const;
+	fruits: Fruit[] = [{ name: "Lemon" }, { name: "Lime" }, { name: "Apple" }];
+
+	add(event: MatChipInputEvent): void {
+		const value = (event.value || "").trim();
+
+		// Add our fruit
+		if (value) {
+			this.fruits.push({ name: value });
+		}
+
+		// Clear the input value
+		event.chipInput!.clear();
+	}
+
+	remove(fruit: Fruit): void {
+		const index = this.fruits.indexOf(fruit);
+
+		if (index >= 0) {
+			this.fruits.splice(index, 1);
+		}
+	}
+
+	edit(fruit: Fruit, event: MatChipEditedEvent) {
+		const value = event.value.trim();
+
+		// Remove fruit if it no longer has a name
+		if (!value) {
+			this.remove(fruit);
+			return;
+		}
+
+		// Edit existing fruit
+		const index = this.fruits.indexOf(fruit);
+		if (index >= 0) {
+			this.fruits[index].name = value;
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,14 @@ import { NoteListComponent } from "./components/note-list/note-list.component";
 import { NoteComponent } from "./components/note/note.component";
 import { FormsModule } from "@angular/forms";
 
+import { MatChipsModule } from "@angular/material/chips";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { NgFor } from "@angular/common";
+
 @NgModule({
 	declarations: [AppComponent, DetailViewComponent, NoteListComponent, NoteComponent],
-	imports: [BrowserModule, HttpClientModule, FormsModule],
+	imports: [BrowserModule, HttpClientModule, FormsModule, MatFormFieldModule, MatChipsModule, NgFor, MatIconModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
